fe950f0b77383d00b68b46e42be97477f59b3a8e
MalcolmDahling <mac.dahling@gmail.com>
2021-12-01 21:05:48 +0100

    crap still not working properly

--- main.js
+++ main.js
@@ -2,20 +2,54 @@ let LS = JSON.parse(localStorage.getItem('names'));
 
 if(LS){
     for(let i = 0; i < LS.length; i++){
-        document.body.innerHTML += '<div>'+LS[i]+'</div>';
+        addName( LS[i], i ); //stored name and index
     }
 }
+
+else{
+    LS = [];
+}
+
+
+
+
 
 
 document.getElementById('save').addEventListener('click', function(){
 
-    if(LS){
-        LS.push(document.getElementById('text').value);
-    }
-
-    else{
-        LS = [document.getElementById('text').value];
-    }
+    LS.push(document.getElementById('text').value);
+    addName( LS[LS.length -1], LS.length-1 ); //stored name and index
 
     localStorage.setItem('names', JSON.stringify(LS));
+
+    document.getElementById('text').value = '';
+    addEventListeners();
 });
+
+
+
+
+
+
+
+
+function addEventListeners(){
+    for(let i = 0; i < LS.length; i++){
+        document.getElementById(i).addEventListener('click', function(){
+            LS.splice(i, 1);
+            localStorage.setItem('names', JSON.stringify(LS));
+            document.getElementById(i).parentElement.remove();
+        });
+    }
+}
+addEventListeners();
+
+
+
+
+
+
+
+function addName(name, index){
+    document.body.insertAdjacentHTML('beforeend', '<div>'+name+' <button id="'+index+'">Delete</button> </div>');
+}
